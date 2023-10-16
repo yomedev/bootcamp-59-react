376ed05d849e208a22270179bb74d1e71e8ae693
Yaroslav <yomelianenkowork@gmail.com>
2023-10-16 11:02:03 +0300

    module-7-middleware

--- src/pages/ExercisesPage/MiddlewarePage/MiddlewarePage.jsx
+++ src/pages/ExercisesPage/MiddlewarePage/MiddlewarePage.jsx
@@ -1,10 +1,15 @@
+import { useDispatch } from "react-redux";
 import { getArticlesService } from "../../../services/articlesServices";
+import { articlesThunk } from "../../../redux/store";
 
 export const MiddlewarePage = () => {
+  const dispatch = useDispatch();
 
   const handleFetch = () => {
-    getArticlesService()
-  }
+    dispatch({ type: "test" });
+    dispatch(articlesThunk());
+    getArticlesService();
+  };
 
   return (
     <button className="btn btn-primary my-4" onClick={handleFetch}>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,33 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { counterReducer } from "./counter/counterReducer";
 import { productsReducer } from "./products/productsReducer";
+import { getArticlesService } from "../services/articlesServices";
+import thunk from "redux-thunk";
 
 const reducer = combineReducers({
   quantity: counterReducer,
   products: productsReducer,
 });
 
-export const store = createStore(reducer, composeWithDevTools());
+// const middleware = (store) => (next) => (action) => {
+//   if (typeof action === "function") {
+//     action(store.dispatch);
+//   }
+//   return next(action);
+// };
+
+export const articlesThunk = () => async (dispatch) => {
+  dispatch({ type: "loading" });
+  try {
+    const data = await getArticlesService();
+    dispatch({ type: "success", payload: data });
+  } catch (error) {
+    dispatch({ type: "error" });
+  }
+};
+
+export const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
